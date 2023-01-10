8879fabf1872da183a3dadfec0d6bf4a516b4024
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-10 15:19:22 -0800

    borders and backgrounds

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
+import background from './background.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `URL(${background})` }}>
       <Header />
       <Main />
       <Footer />
