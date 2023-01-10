97c9c330a48efdb8f6d7d44bd1a91f8c35250423
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-10 14:39:54 -0800

    playing with css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,5 +2,9 @@ import React from 'react';
 import './Footer.css';
 import footer from '../../footer.jpg';
 export default function Footer() {
-  return <footer style={{ backgroundImage: `URL(${footer})` }}>Thanks for visiting</footer>;
+  return (
+    <footer style={{ backgroundImage: `URL(${footer})` }}>
+      <h3>Thanks for visiting</h3>
+    </footer>
+  );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,5 +2,9 @@ import React from 'react';
 import './Header.css';
 import footer from '../../footer.jpg';
 export default function Header() {
-  return <header style={{ backgroundImage: `URL(${footer})` }}>Zodiac Signs</header>;
+  return (
+    <header style={{ backgroundImage: `URL(${footer})` }}>
+      <h2 className="header">Zodiac Signs</h2>
+    </header>
+  );
 }
